630b00b93fd69f621316e449f86289ca8eda0b5e
sittranlong <sittranlong@gmail.com>
2024-03-15 11:41:07 +0700

    Đẩy Giao Diện Quản Trị Sản Phẩm

---
+++ BanGiaySneaker/src/main/java/poly/edu/nhom1/bangiaysneaker/BanGiaySneaker.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package poly.edu.nhom1.bangiaysneaker;
+
+/**
+ *
+ * @author TieuLong
+ */
+public class BanGiaySneaker {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
